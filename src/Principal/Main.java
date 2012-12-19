28357dabe923b9b2592dcffea2515d521f9587dc
Maike <maike-ps@hotmail.com>
2012-12-19 21:54:26 -0200

    classe main arrumada

--- src/Principal/Main.java
+++ src/Principal/Main.java
@@ -4,16 +4,34 @@
  */
 package Principal;
 
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
 /**
  *
  * @author Lucas
  */
-public class Main {
+public class Main extends StateBasedGame{
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws SlickException {
+        AppGameContainer app = new AppGameContainer(new Main("The Warrior's Way"));
+        app.setDisplayMode(800, 600, false);
+        app.setTargetFrameRate(60);
+        
+        app.start();
+    }
+
+    @Override
+    public void initStatesList(GameContainer gc) throws SlickException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    public Main(String title){
+        super(title);
     }
 }
