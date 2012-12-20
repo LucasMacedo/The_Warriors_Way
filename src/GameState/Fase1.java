c7333e6c12fa60906217515d4b3ec6519aa6e042
LucasMacedo <lucas_sm2007@hotmail.com>
2012-12-19 22:13:40 -0200

    Inicio dia 19/12

---
+++ src/GameState/Fase1.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GameState;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+/**
+ *
+ * @author Lucas
+ */
+public class Fase1 extends BasicGameState{
+    
+    public static final int ID = 2;
+    
+    
+
+    @Override
+    public int getID() {
+       return ID;
+    }
+
+    @Override
+    public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void render(GameContainer gc, StateBasedGame sbg, Graphics grphcs) throws SlickException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void update(GameContainer gc, StateBasedGame sbg, int i) throws SlickException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }  
+}
---
+++ src/GameState/MenuPrincipal.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GameState;
+
+/**
+ *
+ * @author Lucas
+ */
+public class MenuPrincipal {
+    
+}
