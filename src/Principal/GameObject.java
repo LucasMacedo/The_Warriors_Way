d8cf3bc1bda9a5f7db8bc95b800669a89f7b2ad9
Maike <maike-ps@hotmail.com>
2012-12-19 23:41:22 -0200

    classe player

--- src/Principal/GameObject.java
+++ src/Principal/GameObject.java
@@ -1,13 +1,20 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package Principal;
 
-/**
- *
- * @author Maike
- */
-public class GameObject {
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.state.StateBasedGame;
+
+public abstract class GameObject {
+    protected int x;
+    protected int y;
     
+    public abstract void update(GameContainer gc, StateBasedGame game, int delta);
+    public abstract void render(GameContainer gc, StateBasedGame game, Graphics g);
+    
+    public int getX(){
+        return this.x;
+    }
+    public int getY(){
+        return this.y;
+    }
 }
--- src/Principal/Player.java
+++ src/Principal/Player.java
@@ -1,13 +1,34 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package Principal;
 
-/**
- *
- * @author Maike
- */
-public class Player {
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Player extends GameObject{
+
+    String nome;
+    Image imagem;
+    
+    public Player(int x, int y){
+        try {
+            this.imagem = new Image("resources/player.png");
+        } catch (SlickException ex) {
+            ex.printStackTrace();
+        }
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public void update(GameContainer gc, StateBasedGame game, int delta) {
+        
+    }
+
+    @Override
+    public void render(GameContainer gc, StateBasedGame game, Graphics g) {
+        this.imagem.draw(this.x, this.y);
+    }
     
 }
